84e2112990ec79428a20d7df1f1f67a0df9dd185
William Holecek <ramholecek@gmail.com>
2014-12-11 16:38:36 +0100

    random push

--- src/Git_Holecek_Keles_Kogler/TA_LogicClass_Holecek.java
+++ src/Git_Holecek_Keles_Kogler/TA_LogicClass_Holecek.java
@@ -3,10 +3,13 @@ package Git_Holecek_Keles_Kogler;
 public class TA_LogicClass_Holecek implements TA_LogicClass{
 
 	@Override
-	public indexOnOff[] whatToChange() {
+	public indexOnOff[] whatToChange(int index) {
+		indexOnOff[] temp = new indexOnOff[25];
+		for(int i=0; i<25; i++) {
+			temp[i] = 
+		}
 		// TODO Auto-generated method stub
-		
-		return null;
+		return temp;
 	}
 
 	@Override
