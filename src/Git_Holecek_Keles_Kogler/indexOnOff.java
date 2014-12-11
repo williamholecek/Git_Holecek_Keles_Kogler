02e442d85f494d122e28c1de158be5c7f34d7578
Philipp Kogler <pkogler@student.tgm.ac.at>
2014-12-11 09:15:56 +0100

    Die Interfaces Logic und View wurden hinzugefügt

---
+++ src/Git_Holecek_Keles_Kogler/indexOnOff.java
@@ -0,0 +1,19 @@
+package Git_Holecek_Keles_Kogler;
+
+public class indexOnOff {
+	private int index;
+	private boolean on;
+	
+	public indexOnOff(int index, boolean on) {
+		this.index = index;
+		this.on = on;
+	}
+	
+	public int getIndex() {
+		return this.index;
+	}
+	
+	public boolean getOn() {
+		return this.on;
+	}
+}
