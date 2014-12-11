c3cfaf53eea28776db3a62c64ec4d3078132372e
Philipp Kogler <pkogler@student.tgm.ac.at>
2014-12-11 09:43:33 +0100

    Controller Class Kogler

---
+++ src/Git_Holecek_Keles_Kogler/TA_ControllerClass_Kogler.java
@@ -0,0 +1,20 @@
+package Git_Holecek_Keles_Kogler;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class TA_ControllerClass_Kogler implements TA_ControllerClass, ActionListener{
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		indexOnOff[] = 
+	}
+
+	@Override
+	public indexOnOff[] getButtons() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
