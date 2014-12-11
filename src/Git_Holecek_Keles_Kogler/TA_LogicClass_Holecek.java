a3a83dbf237fd58a134723323b130e9dbfd48cac
William Holecek <ramholecek@gmail.com>
2014-12-11 16:08:15 +0100

    habe nur implementiert

---
+++ src/Git_Holecek_Keles_Kogler/TA_LogicClass_Holecek.java
@@ -0,0 +1,18 @@
+package Git_Holecek_Keles_Kogler;
+
+public class TA_LogicClass_Holecek implements TA_LogicClass{
+
+	@Override
+	public indexOnOff[] whatToChange() {
+		// TODO Auto-generated method stub
+		
+		return null;
+	}
+
+	@Override
+	public indexOnOff[] allValues() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
